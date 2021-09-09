af336feffdbf4a483719aa36bda5c19835a38657
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-09-08 23:10:06 -0300

    Finalização com Styled Components

--- semana-5/labedin/src/components/CardGrande/CardGrande.js
+++ semana-5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import './CardGrande.css'
+// import './CardGrande.css'
+import styled from 'styled-components';
+
+const BigCardContainer = styled.div`
+display: flex;
+align-items: center;
+border: 1px solid black;
+padding: 20px 10px;
+margin-bottom: 10px;
+height: 200px;
+`
+const ImgBigCard = styled.img`
+width: 70px;
+margin-right: 10px;
+border-radius: 50%;
+`
+
+const BigCardH4 = styled.h4`
+margin-bottom: 15px;
+`
+
+const BigCardDados = styled.div`
+display: flex;
+flex-direction: column;
+justify-items: flex-start;
+`
+
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
+        <BigCardContainer>
+            <ImgBigCard src={ props.imagem } />
+            <BigCardDados>
+                <BigCardH4>{ props.nome }</BigCardH4>
                 <p>{ props.descricao }</p>
-            </div>
-        </div>
+            </BigCardDados>
+        </BigCardContainer>
     )
 }
 
--- semana-5/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana-5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import './CardPequeno.css'
+// import './CardPequeno.css'
+import styled from "styled-components"
+
+const LittleCardContainer = styled.div`
+display: flex;
+align-items: center;
+border: 1px solid black;
+padding: 10px 10px;
+height: 70px;
+`
+const Icone = styled.img`
+width: 40px;
+margin-right: 10px;
+`
+
+const Dados = styled.div`
+display: flex;
+justify-items: flex-end;
+`
+const ParagrafoDados = styled.p`
+padding-left: 10px;
+`
+
 
 const CardPequeno = (props) => {
     return(
-        <div className="littlecard-container">
-            <img src={props.imagem}/>
-            <div>
+        <LittleCardContainer>
+            <Icone src={props.imagem}/>
+            <Dados>
                 <h4>{props.tipo_dado}</h4>
-                <p>{props.dados}</p>
-            </div>
-        </div>
+                <ParagrafoDados>{props.dados}</ParagrafoDados>
+            </Dados>
+        </LittleCardContainer>
     )
 
 }
