68b3e01e3373720e6be90f1bdfc2d58e13e157d7
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-09-10 17:34:29 -0300

    primeiro exercício do dia 10/09

--- semana-5/insta4/src/App.js
+++ semana-5/insta4/src/App.js
@@ -9,27 +9,67 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+
 class App extends React.Component {
+
+  state = {
+    posts:[
+      {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150"
+      },
+      {
+        nomeUsuario: "joãozinho",
+        fotoUsuario: "https://picsum.photos/50/50/",
+        fotoPost: "https://picsum.photos/200/150/"
+      },
+      {
+        nomeUsuario: "mariazinha",
+        fotoUsuario: "https://picsum.photos/50/50//",
+        fotoPost: "https://picsum.photos/200/150//"
+      }
+  
+    ]
+  }
+  
+
   render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'joãozinho'}
-          fotoUsuario={'https://picsum.photos/50/50/'}
-          fotoPost={'https://picsum.photos/200/150/'}
-        />
-        <Post
-          nomeUsuario={'mariazinha'}
-          fotoUsuario={'https://picsum.photos/50/50//'}
-          fotoPost={'https://picsum.photos/200/150//'}
-        />
-      </MainContainer>
-    );
+    const posts = this.state.posts.map((cadaPost)=>{
+      return (
+        <MainContainer key={cadaPost.nomeUsuario}>
+            <Post
+            nomeUsuario={cadaPost.nomeUsuario}
+            fotoUsuario={cadaPost.fotoUsuario}
+            fotoPost={cadaPost.fotoPost} />
+        </MainContainer>
+      )
+
+
+        // <MainContainer>
+        //   <Post
+        //     nomeUsuario={'paulinha'}
+        //     fotoUsuario={'https://picsum.photos/50/50'}
+        //     fotoPost={'https://picsum.photos/200/150'}
+        //   />
+        //   <Post
+        //     nomeUsuario={'joãozinho'}
+        //     fotoUsuario={'https://picsum.photos/50/50/'}
+        //     fotoPost={'https://picsum.photos/200/150/'}
+        //   />
+        //   <Post
+        //     nomeUsuario={'mariazinha'}
+        //     fotoUsuario={'https://picsum.photos/50/50//'}
+        //     fotoPost={'https://picsum.photos/200/150//'}
+        //   />
+        // </MainContainer>
+      // );
+
+      
+    })
+    return(
+      <div>{posts}</div>
+    )
   }
 }
 
