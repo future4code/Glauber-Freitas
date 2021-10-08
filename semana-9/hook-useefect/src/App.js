5e9be7c82408f87e6bed042c6dc7243a2e730cae
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-10-08 10:44:46 -0300

    Aula 32

--- semana-9/hook-useefect/src/App.js
+++ semana-9/hook-useefect/src/App.js
@@ -1,18 +1,40 @@
-
+import axios from 'axios';
 import './App.css';
+import PokeCard from './components/PokeCard/PokeCard';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("")
+  useEffect(()=>{
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res)=>{
+        setPokeList (res.data.results)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  })
+  const changePokemonName = (e) =>{
+    setPokeName(e.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-     
-        <p>
-          TESTE
-        </p>
-        
-      </header>
+      <select onChange={changePokemonName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map((pokemon) =>{
+          return(
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          )
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon = {pokeName}/>}
     </div>
   );
 }
-
 export default App;
