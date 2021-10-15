880c09798f8041267a62bd2286dd86864a8fc61c
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-10-14 22:43:59 -0300

    Adição de rotas

--- semana-10/labex/src/App.js
+++ semana-10/labex/src/App.js
@@ -2,25 +2,40 @@ import './App.css';
 import React from 'react';
 import { IconButton } from '@material-ui/core';
 import HomePage from './pages/HomePage';
-
-import AdbIcon from '@material-ui/icons/Adb';
 import ListTripsPage from './pages/ListTripsPages';
 import AdminHomePage from './pages/AdminHomePage';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
+    <header className="App-header">
         <IconButton>Inicial</IconButton>
         <h1>LabeX</h1>
         <IconButton>
         <button variant="outlined">Login</button>
         </IconButton>
       </header>
-      <HomePage/>
-      <ListTripsPage/>
-      <AdminHomePage/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path = {"/"}>
+          <HomePage/>
+        </Route>
+        <Route exact path={"/listTrips"}>
+          <ListTripsPage/>
+        </Route>
+        <Route exact path ={"/adminPage"}>
+          <AdminHomePage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
     </div>
+      
+      
+      
+      
+    
   );
 }
 
