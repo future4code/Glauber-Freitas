b0d351b308cfa4c89f91f79b464bcb08d2eb7d80
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-10-17 20:30:53 -0300

    Alteração da página de viagens

--- semana-10/labex/src/pages/ListTripsPages.js
+++ semana-10/labex/src/pages/ListTripsPages.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useRequestData } from "../hooks/useRequestData";
 import styled from 'styled-components';
+import {Button} from '@material-ui/core';
+import {useHistory} from "react-router-dom";
 
 const TripCard = styled.div`
     display: flex;
@@ -20,35 +22,38 @@ const Trips = styled.div`
 
 
 function ListTripsPage() {
+  const history = useHistory()
+  const goToCreateTripPage = () => {
+    history.push("/viagens/criar")
+}
+
     const [trips, isLoadingtrips, errorRequest] = useRequestData(
         "https://us-central1-labenu-apis.cloudfunctions.net/labeX/glauber-freitas-maryam/trips"
     );
     console.log(trips, "trips")
     return (
         <Trips>
-    <h1>Lista de Viagens</h1>
-      {/* Caso de loading */}
-    {isLoadingtrips && <h2>Carregando...</h2>}
-      {/* Caso de mensagem de erro */}
-    {!isLoadingtrips && errorRequest && <p>{errorRequest.message}</p>}
-      {/* Caso de retorno vazio  */}
-    {!isLoadingtrips && trips && trips.trips.length === 0 && (
-        <p>Não foi encontrado nenhum resultado </p>
-    )}
-      {/* Caso de sucesso */}
-    {!isLoadingtrips && trips &&
-        trips.trips.map((trip) => 
-        <TripCard key={trip.id}>
-            Nome:{trip.name}<br></br>
-            Descrição: {trip.description}<br></br>
-            Planeta: {trip.planet}<br></br>
-            Duração: {trip.durationInDays}dias<br></br>
-            Data: {trip.date}
-            <hr></hr>
+          <h1>Viagens</h1>
+          <Button variant={'contained'} onClick={goToCreateTripPage}>Criar Viagem</Button>
+          {isLoadingtrips && <h2>Carregando...</h2>}
+          {!isLoadingtrips && errorRequest && <p>{errorRequest.message}</p>}
+          {!isLoadingtrips && trips && trips.trips.length === 0 && (
+          <p>Não foi encontrado nenhum resultado </p>
+           )}
+          {!isLoadingtrips && trips &&
+            trips.trips.map((trip) => 
+            <a href={"/viagens/detalhe"}><TripCard key={trip.id}>
+              <h3>{trip.name}</h3>
+              {/* Descrição: {trip.description}<br></br>
+              Planeta: {trip.planet}<br></br>
+              Duração: {trip.durationInDays}dias<br></br>
+              Data: {trip.date} */}
+              
 
-        </TripCard>
-        )}
-    </Trips>
+            </TripCard>
+            </a>
+            )}
+        </Trips>
     );
 }
 
--- semana-10/labex/src/pages/TripDetailsPage.js
+++ semana-10/labex/src/pages/TripDetailsPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const DetailsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 80vw;
+`
+
+const Trip = styled.div`
+
+`
+const Travellers = styled.div`
+
+`
 
 const TripDetailsPage = () => {
-    return <div>TripDetailsPage</div>
-}
+    return (
+        <DetailsContainer>
+            <Trip>Trips</Trip>
+            <Travellers>Travellers</Travellers>
+
+        </DetailsContainer>
+    )}
 
 export default TripDetailsPage
