8d307b73b9a4b073b511ad91711079af92543841
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-16 15:54:13 -0300

    Exercício 3

--- semana-15/node-package/index.js
+++ semana-15/node-package/index.js
@@ -7,24 +7,35 @@
 // console.log(`Olá ${nome}! você tem ${idade} anos, Em sete anos você terá ${idadeFutura}.`)
 
 // Exercício 2
-const operacao = process.argv[2]
-const valor1 = Number(process.argv[3])
-const valor2 = Number(process.argv[4])
-let result = 0
+// const operacao = process.argv[2]
+// const valor1 = Number(process.argv[3])
+// const valor2 = Number(process.argv[4])
+// let result = 0
 
-switch(operacao){
-    case "add":
-        result = valor1+valor2
-        break
-    case "mult":
-        result = valor1*valor2
-        break
-    case "sub":
-        result = valor1-valor2
-        break
-    case "div":
-        result = valor1/valor2
-        break
-}
+// switch(operacao){
+//     case "add":
+//         result = valor1+valor2
+//         break
+//     case "mult":
+//         result = valor1*valor2
+//         break
+//     case "sub":
+//         result = valor1-valor2
+//         break
+//     case "div":
+//         result = valor1/valor2
+//         break
+// }
 
-console.log(`O resultado é: ${result}`)
+// console.log(`O resultado é: ${result}`)
+
+//Exercício 3
+
+const tarefas = [
+        "Lavar Louça",
+        "Comprar leite"
+        ]
+
+const adicionar = process.argv[2]
+tarefas.push(process.argv[2])
+console.table(tarefas)
