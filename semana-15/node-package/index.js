bdf4de5d2d94bc478820e88878779db85632140f
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-16 15:45:41 -0300

    Exercício 2

--- semana-15/node-package/index.js
+++ semana-15/node-package/index.js
@@ -1,8 +1,30 @@
 //Exercício 1
 // a- usando a propriedade process.argv[] 
-const nome = process.argv[2]
-const idade = Number(process.argv[3])
-const idadeFutura = idade +7 
+// const nome = process.argv[2]
+// const idade = Number(process.argv[3])
+// const idadeFutura = idade +7 
 
-console.log(`Olá ${nome}! você tem ${idade} anos, Em sete anos você terá ${idadeFutura}.`)
+// console.log(`Olá ${nome}! você tem ${idade} anos, Em sete anos você terá ${idadeFutura}.`)
 
+// Exercício 2
+const operacao = process.argv[2]
+const valor1 = Number(process.argv[3])
+const valor2 = Number(process.argv[4])
+let result = 0
+
+switch(operacao){
+    case "add":
+        result = valor1+valor2
+        break
+    case "mult":
+        result = valor1*valor2
+        break
+    case "sub":
+        result = valor1-valor2
+        break
+    case "div":
+        result = valor1/valor2
+        break
+}
+
+console.log(`O resultado é: ${result}`)
