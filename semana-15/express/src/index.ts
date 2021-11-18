ba6ed9b0282a1d00cc387759c852a0f3ff6e80a4
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-18 19:24:17 -0300

    Enpoint para retornar o país pelo id

--- semana-15/express/src/index.ts
+++ semana-15/express/src/index.ts
@@ -2,6 +2,7 @@ import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
 import {countries} from "./data"
+import { country } from './types'
 
 const app: Express = express()
 
@@ -28,6 +29,29 @@ app.get(
         
     })
 
+//ENDPOINT  para retornar o país pelo id
+app.get(
+    "/countries/:id", 
+    (req: Request, res: Response) => {
+    try{
+        const id = req.params.id
+        console.log(id)
+        const result: country | undefined = countries.find((country)=>{
+            return country.id === Number(id)
+        })
+        if (result){
+            res.status(200).send(result)
+        }else {
+            res.status(404).send("País não encontrado")
+        }
+    }catch(err: any) {
+        if(res.statusCode === 200){
+            res.status(500).end()
+        }else {
+            res.end()
+        }
+    }
+})
 
 const server = app.listen(process.env.PORT || 3003, ()=>{
     if(server){
