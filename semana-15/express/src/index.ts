862ad7ab180c6f990b6d73bbdfda9ba2d438660e
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-18 19:08:07 -0300

    Primeiro endpoint para retornar todos os países

--- semana-15/express/src/index.ts
+++ semana-15/express/src/index.ts
@@ -1,11 +1,32 @@
-import express, {Express} from 'express'
+import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
+import {countries} from "./data"
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
+
+//ENDPOINT para retornar todos os países
+app.get(
+    "/countries",
+    (req: Request, res: Response)=>{
+        try{
+            const result = countries.map((country)=>({
+                id: country.id,
+                name: country.name
+            }))
+            res.status(200).send(result)
+        }catch (err: any){
+            if(res.statusCode === 200){
+                res.status(500).end()
+            }else {
+                res.end()
+            }
+        }
+        
+    })
 
 
 const server = app.listen(process.env.PORT || 3003, ()=>{
