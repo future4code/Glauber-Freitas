58a702e376382a89745381ef2c7e3db1e8ca9989
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-19 20:58:34 -0300

    Exercício 4

--- semana-15/apis-rest/src/index.ts
+++ semana-15/apis-rest/src/index.ts
@@ -32,19 +32,19 @@ app.get("/users/search", (req:Request, res: Response) => {
         let userFind: user[] | undefined
         const userType: string = req.query.usertype as string
         const userName: string = req.query.userName as string
-        if(userType && userName){
+        if(userType !== "ADMIN" && userType !== "NORMAL" && userType !== undefined ){
+            errorCode = 400
+            throw new Error("Parâmetro inválido")
+        }else if(userType && userName){
             userFind = users.filter(user => user.type === userType && user.name === userName)
         }else if(userType && !userName){
             userFind = users.filter(user => user.type === userType)
-            console.log(userFind)
         }else if(!userType && userName){
             userFind = users.filter(user => user.name === userName)
-            console.log(userFind)
         }else{
             throw new Error("Inclua algum parâmetro de busca")
         }
-        
-        if(!userFind){
+        if(userFind.length===0){
             errorCode = 404
             throw new Error('Nenhum usuário encontrado')
         }
@@ -53,6 +53,29 @@ app.get("/users/search", (req:Request, res: Response) => {
         res.status(errorCode).send({message: err.message})
     }
 }) 
+
+app.put("/users", (req: Request, res: Response) => {
+    let errorCode = 400
+    try{
+        const {id, name, email, type, age} = req.body
+        console.log({id, name, email, type, age})
+        if(!id || !name || !email || !type || !age){
+            errorCode = 422
+            throw new Error('Verifique os campos')
+        }
+        const newUser: user = {
+            id,
+            name,
+            email,
+            type,
+            age
+        }
+        users.push(newUser)
+        res.status(200).send({message: "Usuário criado"})
+    }catch(err: any){
+        res.status(errorCode).send({merssage: err.message})
+    }
+})
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if(server){
