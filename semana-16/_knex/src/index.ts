6077fa9ed69766689aa63fe7404abd24db5ee688
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-25 18:43:28 -0300

    Exercício 1

--- semana-16/_knex/src/index.ts
+++ semana-16/_knex/src/index.ts
@@ -1,11 +1,52 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
+import connection from "./connections";
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+});
+
+const findActorByName = async (name: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = "${name}"
+  `)
+  return result[0]
+}
+
+app.get("/atores/:name", async (req: Request, res: Response) => {
+  try{
+    const name = req.params.name
+    console.log(name)
+    const result = findActorByName(name)
+    res.status(200).send({resultado: result})
+  }catch(err: any){
+    res.status(500).send({message: err.message})
+  }
+})
+
+const countGender = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+      SELECT COUNT(*) FROM Actor WHERE gender = "${gender}"
+  `)
+  return result[0]
+}
+
+app.get("/atores", async (req: Request, res: Response) => {
+  try{
+      const count = await countGender(req.query.gender as string)
+      console.log(count)
+      res.status(200).send( {quantity: count})
+  }catch (err: any){
+      res.status(400).send({message: err.message})
+  }
+})
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if(server){
